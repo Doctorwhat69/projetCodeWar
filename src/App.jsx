78531f63831ba962bd7c187ccb0fe04c548c13c4
Doctorwhat69 <thoblanc.tb@gmail.com>
2025-04-07 18:41:42 +0200

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import KataList from './components/KataList'
 
 function App() {
 
@@ -8,6 +9,8 @@ function App() {
       <p>Plonger dans l’univers de CodeWars, c’est relever chaque jour de nouveaux défis en algorithmie et en logique. 
         À travers ces exercices, j’affûte mes compétences en programmation tout en m’amusant. Voici une sélection de katas que j’ai résolus, 
         allant des plus simples aux plus corsés. Chaque ligne de code est un pas de plus vers la maîtrise !</p>
+
+        <KataList/>
     </>
   )
 }
---
+++ src/components/KataList.jsx
@@ -0,0 +1,32 @@
+import React,{ useEffect } from "react";
+import data from "../assets/data.json";
+import Prism from "prismjs";
+import "prismjs/themes/prism-tomorrow.css";
+import "prismjs/components/prism-javascript";
+import "../index.css";
+
+const KataList = () => {
+    useEffect(() => {
+        Prism.highlightAll(); 
+      }, []);
+
+    return (
+        <div className="kata-container">
+          <h1 className="kata-title">🚀 Mes Katas CodeWars</h1>
+          <div className="kata-list">
+            {data.map((kata, index) => (
+              <div className="kata-card" key={index}>
+                <h2 className="kata-name">{kata.name}</h2>
+                <p className="kata-date">{kata.date}</p>
+                <p className="kata-description">{kata.description}</p>
+                <pre className="kata-code">
+                  <code className="language-javascript">{kata.code}</code>
+                </pre>
+              </div>
+            ))}
+          </div>
+        </div>
+      );
+    };
+
+export default KataList;
